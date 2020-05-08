ac876fd387b17c7dcc4553f37f0636711cfd2e01
Anh Tran <chirstmax@gmail.com>
2020-05-07 22:00:52 -0400

    add code

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../svg/logo.svg';
+import '../css/App.css';
+import Navigation from "../components/Navigation";
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
